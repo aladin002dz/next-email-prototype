c49d930d00a6da5a8704c69d7706af2b8a596bd9
aladin002dz <mahfoudh.arous@gmail.com>
2025-03-04 15:05:25 +0100

    adding and displaying a logo inside the email

--- app/emails/welcome-email.tsx
+++ app/emails/welcome-email.tsx
@@ -26,7 +26,7 @@ export const WelcomeEmail = ({
       <Body style={main}>
         <Container style={container}>
           <Img
-            src="https://github.com/aladin002dz/next-email-prototype/blob/main/public/react_logo.svg?raw=true"
+            src="https://github.com/aladin002dz/next-email-prototype/blob/main/public/marodev-logo.png?raw=true"
             width="150"
             height="50"
             alt="Logo"
